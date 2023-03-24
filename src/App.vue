a57eec7a6a41de8796509d062b28bf8e7606557b
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-03-24 19:07:38 -0300

    :bug: fix: nav indicator

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,10 @@ export default {
 <template>
   <div class="mobile:bg-sidebar-mobile bg-cover bg-no-repeat mobile:h-[172px]">
     <nav class="text-center text-white pt-8">
-      <Sidebar page="1" :class="$route.name == 'page1' ? 'bg-blue-light text-blue-marine' : ''"/>
-      <Sidebar page="2" :class="$route.name == 'page2' ? 'bg-blue-light text-blue-marine' : ''"/>
-      <Sidebar page="3" :class="$route.name == 'page3' ? 'bg-blue-light text-blue-marine' : ''"/>
-      <Sidebar page="4" :class="$route.name == 'page4' || 'page5' ? 'bg-blue-light text-blue-marine' : ''"/>
+      <Sidebar page="1" :class="$route.name === 'page1' ? 'bg-blue-light text-blue-marine' : ''"/>
+      <Sidebar page="2" :class="$route.name === 'page2' ? 'bg-blue-light text-blue-marine' : ''"/>
+      <Sidebar page="3" :class="$route.name === 'page3' ? 'bg-blue-light text-blue-marine' : ''"/>
+      <Sidebar page="4" :class="$route.name === 'page4' ? 'bg-blue-light text-blue-marine' : $route.name === 'page5' ? 'bg-blue-light text-blue-marine' : ''"/>
     </nav>
   </div>
 
