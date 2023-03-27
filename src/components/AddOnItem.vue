dffcd8aa9150f2256c46bbde11c518b3ef297c66
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-03-26 22:18:14 -0300

    :bug: fix: add-ons page

--- src/components/AddOnItem.vue
+++ src/components/AddOnItem.vue
@@ -2,16 +2,14 @@
 defineProps({
     title: "",
     description: "",
-    monthlyPrice: "",
-    yearlyPrice: "",
-    groupName: ""
+    idName: ""
 })
 </script>
 
 <template>
     <div class="flex items-center">
-        <input id="checkbox" type="checkbox" class="accent-blue-purplish mr-[-2rem] ml-[2rem] lg:mr-[-3rem] lg:ml-[3rem] w-4 h-4 lg:w-5 lg:h-5 peer z-10" :aria-label="`checkbox for ` + title">
-        <label for="checkbox" class="pl-14 lg:pl-20 p-3 lg:p-4 my-2 flex items-center justify-between w-full peer-checked:bg-magnolia peer-checked:border-blue-purplish border border-gray-light hover:cursor-pointer rounded-md">
+        <input :id="`${idName}`" type="checkbox" class="accent-blue-purplish mr-[-2rem] ml-[2rem] lg:mr-[-3rem] lg:ml-[3rem] w-4 h-4 lg:w-5 lg:h-5 peer z-10" :aria-label="`checkbox for ` + title">
+        <label :for="`${idName}`" class="pl-14 lg:pl-20 p-3 lg:p-4 my-2 flex items-center justify-between w-full peer-checked:bg-magnolia peer-checked:border-blue-purplish border border-gray-light hover:cursor-pointer rounded-md">
             <div>
                 {{ title }}
                 <p class="text-gray-cool text-sm font-medium">{{ description }}</p>
--- src/views/AddOnsView.vue
+++ src/views/AddOnsView.vue
@@ -28,15 +28,15 @@ export default {
         <template #title>Pick add-ons</template>
         <template #description>Please provide your name, email address and phone number.</template>
         <template #body>
-            <AddOnItem title="Online service" description="Access to multiplayer games">
+            <AddOnItem title="Online service" description="Access to multiplayer games" idName="onlineService">
                 <template #monthlyPrice v-if="!yearly">+$1/mo</template>
                 <template #yearlyPrice v-else>+$10/yr</template>
             </AddOnItem>
-            <AddOnItem title="Larger storage" description="Extra 1TB of cloud save">
+            <AddOnItem title="Larger storage" description="Extra 1TB of cloud save" idName="largerStorage">
                 <template #monthlyPrice v-if="!yearly">+$2/mo</template>
                 <template #yearlyPrice v-else>+$20/yr</template>
             </AddOnItem>
-            <AddOnItem title="Customizable profile" description="Custom theme on your profile">
+            <AddOnItem title="Customizable profile" description="Custom theme on your profile" idName="customProfile">
                 <template #monthlyPrice v-if="!yearly">+$2/mo</template>
                 <template #yearlyPrice v-else>+$20/yr</template>
             </AddOnItem>
