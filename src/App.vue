94236a65c502c70b2c90d9511a6e3a2b70f0afb1
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-03-26 22:37:40 -0300

    :bug: fix: mobile layout navigation

--- src/App.vue
+++ src/App.vue
@@ -17,17 +17,17 @@ export default {
 <template>
   <div class="bg-sidebar-mobile bg-cover bg-no-repeat h-[172px] lg:hidden">
     <div class="flex justify-center text-white pt-8">
-      <IconSidebar page="1" :class="$route.name === 'page1' ? 'bg-blue-light text-blue-marine' : ''"/>
-      <IconSidebar page="2" :class="$route.name === 'page2' ? 'bg-blue-light text-blue-marine' : ''"/>
-      <IconSidebar page="3" :class="$route.name === 'page3' ? 'bg-blue-light text-blue-marine' : ''"/>
-      <IconSidebar page="4" :class="$route.name === 'page4' ? 'bg-blue-light text-blue-marine' : $route.name === 'page5' ? 'bg-blue-light text-blue-marine' : ''"/>
+      <IconSidebar page="1" :extraClasses="$route.name === 'page1' ? ' bg-blue-light text-blue-marine' : ' text-white'"/>
+      <IconSidebar page="2" :extraClasses="$route.name === 'page2' ? ' bg-blue-light text-blue-marine' : ' text-white'"/>
+      <IconSidebar page="3" :extraClasses="$route.name === 'page3' ? ' bg-blue-light text-blue-marine' : ' text-white'"/>
+      <IconSidebar page="4" :extraClasses="$route.name === 'page4' ? ' bg-blue-light text-blue-marine' : $route.name === 'page5' ? ' bg-blue-light text-blue-marine' : ' text-white'"/>
     </div>
   </div>
   
   <div class="hidden lg:block rounded-tl-xl rounded-bl-xl bg-white p-4 h-3/4">
     <div class="bg-sidebar-desktop rounded-xl flex flex-col bg-cover w-full h-full pt-6 pl-6">
         <Sidebar title="Step 1" description="Your Info">
-        <IconSidebar page="1" :extraClasses="$route.name === 'page1' ? ' bg-blue-light text-blue-marine' : ' text-white'"/>
+          <IconSidebar page="1" :extraClasses="$route.name === 'page1' ? ' bg-blue-light text-blue-marine' : ' text-white'"/>
         </Sidebar>
         <Sidebar title="Step 2" description="Select Plan">
           <IconSidebar page="2" :extraClasses="$route.name === 'page2' ? ' bg-blue-light text-blue-marine' : ' text-white'"/>
